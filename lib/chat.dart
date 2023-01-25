3da3af13a1c036ab0b431a2c2fb1198d66904bde
Baaav23 <bavlybadry123@gmail.com>
2023-01-25 18:48:33 +0200

    coding message statelsess widget and api model

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,4 +1,48 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
+
+class Chat extends StatefulWidget {
+  const Chat({super.key});
+
+  @override
+  State<Chat> createState() => _Chat();
+}
+
+class _Chat extends State<Chat> {
+  Future<HttpClientResponse> sendRequest(String prompt) async {
+    final hclint = HttpClient();
+    final hreq = await hclint
+        .postUrl(Uri.parse('https://api.openai.com/v1/completions'));
+    hreq.headers.set('Content-Type', 'application/json');
+    hreq.headers.set('Authorization', 'Bearer $ApiKey');
+    hreq.add(utf8.encode(json.encode({
+      'model': 'text-davinci-003',
+      'prompt': prompt,
+      'max_tokens': 2000,
+      'stop': '',
+      'temperature': 0.7,
+    })));
+    final res = await hreq.close();
+    return res;
+  }
+
+  static const ApiKey = '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';
+
+  void send(String Text) {
+    print(Text + "Controller");
+
+    Message message = Message(
+      text: Text,
+      recive: 'You',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
 
 class Message extends StatelessWidget {
   final String text;
@@ -21,6 +65,20 @@ class Message extends StatelessWidget {
                   child: Text(recive[0]),
                 ),
               ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  recive,
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                Center(
+                  child: Text(
+                    text,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
