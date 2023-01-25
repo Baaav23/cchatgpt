47b22e1eff0cd7fbfd475a3d0a4ccdef292d6d8a
Baaav23 <bavlybadry123@gmail.com>
2023-01-25 18:33:42 +0200

    create designe

--- lib/chat.dart
+++ lib/chat.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Message extends StatelessWidget {
+  final String text;
+  final String recive;
+
+  const Message({super.key, required this.text, required this.recive});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: Container(
+                margin: const EdgeInsets.only(right: 10.0),
+                child: CircleAvatar(
+                  child: Text(recive[0]),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
